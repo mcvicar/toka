a8545792677385cc4e895618a380bf33ba4f37c9
mcvicar <pete@recursivelymade.com>
2020-07-19 12:22:41 +0100

    Add some content to the error pages

---
+++ src/views/error/404.vue
@@ -0,0 +1,8 @@
+<template>
+<div>
+<h1>You've hit a 404</h1>
+<h2>I can see that your lost</h2>
+<h3>But that's OK</h3>
+<h4>We'll get you <a href="/">home</a></h4>
+</div>
+</template>
---
+++ src/views/error/500.vue
@@ -0,0 +1,10 @@
+<template>
+<div class="edit">
+<h1>Ok this is bad</h1>
+<h2>How bad?</h2>
+<h3>Bad, bad.</h3>
+<h4>Like 500 error bad?</h4>
+<h5>Yep</h5>
+<h6>Oh that's bad</h6>
+</div>
+</template>
